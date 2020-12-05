676b131026993e9e088b3e8951377481b061a362
shailendra chauhan <shailendra.chauhan@yash.com>
2020-12-05 19:40:43 +0530

    first commit

---
+++ limits-service/limits-service/src/main/java/com/practice/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,19 @@
+package com.practice.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.practice.microservices.beans.LimitConfiguration;
+
+@RestController("limit")
+public class LimitsConfigurationController {
+
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retreiveLimitsFromConfiguration() {
+		return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
+	}
+}
